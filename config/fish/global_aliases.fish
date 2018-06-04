f235662f481f3dc26db785ffec1769ea2deae720
Andrew Meyer <ameyer@servercentral.com>
2018-06-04 11:33:46 -0500

    break out global aliases

---
+++ config/fish/global_aliases.fish
@@ -0,0 +1,7 @@
+alias aliases "cat ~/.config/fish/aliases.fish"
+alias gpg "gpg2"
+alias gs "git status"
+alias gf "git fetch"
+alias gd "git diff"
+alias gds "git diff --staged"
+alias fixpaste 'printf "\e[?2004l"'
