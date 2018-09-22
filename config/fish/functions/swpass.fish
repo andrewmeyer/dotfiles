cc1eae4ece2fdbd3c40f0eb10b26873b103fabd3
Andrew Meyer <ameyer@servercentral.com>
2018-09-21 22:49:55 -0500

    do it right

--- config/fish/functions/swpass.fish
+++ config/fish/functions/swpass.fish
@@ -1,9 +1,10 @@
 function swpass
   if count $argv > /dev/null
     echo "setting pass to use $argv"
+    set -e PASSWORD_STORE_DIR
     set -g -x PASSWORD_STORE_DIR $argv
   else
-    echo "setting pass to use ~/.password-store"
-    set -g -x PASSWORD_STORE_DIR ~/.password-store
+    echo "unsetting PASSWORD_STORE_DIR ( ~/.password-store)"
+    set -e PASSWORD_STORE_DIR
   end
 end
--- config/fish/global_aliases.fish
+++ config/fish/global_aliases.fish
@@ -5,4 +5,3 @@ alias gf "git fetch"
 alias gd "git diff"
 alias gds "git diff --staged"
 alias fixpaste 'printf "\e[?2004l"'
-alias pass ' pass'
